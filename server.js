b0ad581ce4d2235ae37e60c868a3a20da6d30bf1
BBishop <brad.bishop68@gmail.com>
2016-01-15 09:15:17 -1000

    Adds global search for word in filter function, so can find and replace multiple instances of a word in a string

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ server.use(bodyParser.urlencoded({extended: true}));
 //with new ones
 function filterOne(req, res, next) {
   mess = req.body.message;
-  mess = mess.replace(/selfie/i, 'self-portrait')
-              .replace(/yummers/i, 'delicious')
-              .replace(/outchea/i, 'are out here')
-              .replace(/bruh/i, 'wow')
-              .replace(/doge/i, 'pug')
-              .replace(/cilantro/i, 'soap')
-              .replace(/bae/i, 'loved one')
-              .replace(/swag/i, 'style')
-              .replace(/yolo/i, 'carpe diem');
+  mess = mess.replace(/selfie/gi, 'self-portrait')
+              .replace(/yummers/gi, 'delicious')
+              .replace(/outchea/gi, 'are out here')
+              .replace(/bruh/gi, 'wow')
+              .replace(/doge/gi, 'pug')
+              .replace(/cilantro/gi, 'soap')
+              .replace(/bae/gi, 'loved one')
+              .replace(/swag/gi, 'style')
+              .replace(/yolo/gi, 'carpe diem');
   next();
 }
 //This gets called when post with route 'message' is called.  'filterone' is the
