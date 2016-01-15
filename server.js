7e3f5f92054a1551478530bb74f7e03d1a345f6d
BBishop <brad.bishop68@gmail.com>
2016-01-14 20:15:29 -1000

    Adds comments

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 var mess = '';
 server.use(bodyParser.urlencoded({extended: true}));
 
-
+//middleware function that searches string and replaces particular words
+//with new ones
 function filterOne(req, res, next) {
   mess = req.body.message;
   mess = mess.replace(/selfie/i, 'self-portrait')
@@ -18,7 +19,8 @@ function filterOne(req, res, next) {
               .replace(/yolo/i, 'carpe diem');
   next();
 }
-
+//This gets called when post with route 'message' is called.  'filterone' is the
+//middleware function that is called before the post callback function is ran
 server.post('/message', filterOne, function (req, res) {
   res.status(200).send(mess);
 
